5138ef93171bc4b5d123c7abf4f028b2e3dee1e7
NoahDClark <115905628+NoahDClark@users.noreply.github.com>
2024-04-27 18:59:38 -0400

    Update firebaseConfig.ts

--- Chatbot User Interface (React Native)/firebaseConfig.ts
+++ Chatbot User Interface (React Native)/firebaseConfig.ts
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ6NKasM5hkYwB_lRodBg0PFuf-v99Gws",
-  authDomain: "awesome-project-noah.firebaseapp.com",
-  projectId: "awesome-project-noah",
-  storageBucket: "awesome-project-noah.appspot.com",
-  messagingSenderId: "462808501703",
-  appId: "1:462808501703:web:a7698317398b387edf5de2",
-  measurementId: "G-TJTH4LKR1N"
+  //Config deleted for privacy reasons
 };
 
 // Initialize Firebase
